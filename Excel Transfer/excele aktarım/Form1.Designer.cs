431f16af126c4d4baff9b4caa3188fb612c9f0e9
yasindalkilic <ysndlklc66@hotmail.com>
2017-07-02 18:28:35 +0300

    Excel Transfer
    Excel Transfer

---
+++ Excel Transfer/excele aktarım/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace excele_aktarım
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dersDataSet = new excele_aktarım.dersDataSet();
+            this.dersDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dersDataSetBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.soyadDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adresDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dersTableAdapter = new excele_aktarım.dersDataSetTableAdapters.dersTableAdapter();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dersDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dersDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dersDataSetBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dersBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dersDataSet
+            // 
+            this.dersDataSet.DataSetName = "dersDataSet";
+            this.dersDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dersDataSetBindingSource
+            // 
+            this.dersDataSetBindingSource.DataSource = this.dersDataSet;
+            this.dersDataSetBindingSource.Position = 0;
+            // 
+            // dersDataSetBindingSource1
+            // 
+            this.dersDataSetBindingSource1.DataSource = this.dersDataSet;
+            this.dersDataSetBindingSource1.Position = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.adDataGridViewTextBoxColumn,
+            this.soyadDataGridViewTextBoxColumn,
+            this.telDataGridViewTextBoxColumn,
+            this.adresDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.dersBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(-7, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(560, 162);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // adDataGridViewTextBoxColumn
+            // 
+            this.adDataGridViewTextBoxColumn.DataPropertyName = "ad";
+            this.adDataGridViewTextBoxColumn.HeaderText = "ad";
+            this.adDataGridViewTextBoxColumn.Name = "adDataGridViewTextBoxColumn";
+            // 
+            // soyadDataGridViewTextBoxColumn
+            // 
+            this.soyadDataGridViewTextBoxColumn.DataPropertyName = "soyad";
+            this.soyadDataGridViewTextBoxColumn.HeaderText = "soyad";
+            this.soyadDataGridViewTextBoxColumn.Name = "soyadDataGridViewTextBoxColumn";
+            // 
+            // telDataGridViewTextBoxColumn
+            // 
+            this.telDataGridViewTextBoxColumn.DataPropertyName = "tel";
+            this.telDataGridViewTextBoxColumn.HeaderText = "tel";
+            this.telDataGridViewTextBoxColumn.Name = "telDataGridViewTextBoxColumn";
+            // 
+            // adresDataGridViewTextBoxColumn
+            // 
+            this.adresDataGridViewTextBoxColumn.DataPropertyName = "adres";
+            this.adresDataGridViewTextBoxColumn.HeaderText = "adres";
+            this.adresDataGridViewTextBoxColumn.Name = "adresDataGridViewTextBoxColumn";
+            // 
+            // dersBindingSource
+            // 
+            this.dersBindingSource.DataMember = "ders";
+            this.dersBindingSource.DataSource = this.dersDataSet;
+            // 
+            // dersTableAdapter
+            // 
+            this.dersTableAdapter.ClearBeforeFill = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(374, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 274);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dersDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dersDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dersDataSetBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dersBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource dersDataSetBindingSource;
+        private dersDataSet dersDataSet;
+        private System.Windows.Forms.BindingSource dersDataSetBindingSource1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource dersBindingSource;
+        private dersDataSetTableAdapters.dersTableAdapter dersTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn soyadDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adresDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Excel Transfer/excele aktarım/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using excel = Microsoft.Office.Interop.Excel;
+
+namespace excele_aktarım
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            // TODO: This line of code loads data into the 'dersDataSet.ders' table. You can move, or remove it, as needed.
+            this.dersTableAdapter.Fill(this.dersDataSet.ders);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            excel.Application dosya = new excel.Application();//excel acar
+            dosya.Visible = true;//exceli gösterir 
+            object a =Type.Missing;
+            excel.Workbook kitap = dosya.Workbooks.Add(a);//calısma sayfası olusturur.
+            excel.Worksheet sayfa = (excel.Worksheet)kitap.Sheets[1];//calısma alanı çalısma sayfası 1 rakamı kacıncı sayfada calısacaksak
+            int sutun = 1;//excele yazdıracagımız satır
+            int satır = 1;//excele yazdıracagımız sutun
+            for (int i = 0; i < dataGridView1.Columns.Count; i++)//5 alan varsa 5 dönecek
+            {
+                excel.Range hücre = (excel.Range)sayfa.Cells[satır, sutun + i];//alan hangi alan hücre biri sıfırdan digeri biroldugu için +2
+                hücre.Value2 = dataGridView1.Columns[i].HeaderText;//alanın o degerine
+            }
+            satır++;
+            for (int z = 0; z < dataGridView1.Rows.Count; z++)
+            {
+                for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                {
+                    try
+                    {
+                        excel.Range hücre = (excel.Range)sayfa.Cells[satır + z, sutun + i];
+                        hücre.Value2 = dataGridView1[i, z].Value;
+                    }
+                    catch (Exception AAA)
+                    {
+
+                        MessageBox.Show(AAA.ToString());
+                    }
+                    
+                }
+            }
+        }
+
+       
+    }
+}
